17c34e8aadf257ceb2ffd47732b2acd74ead7097
gregori bruno felicio <gregory.wow@hotmail.com>
2021-04-20 20:09:12 -0300

    fix login form group import error

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login.routing.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    LoginRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class LoginModule {}
