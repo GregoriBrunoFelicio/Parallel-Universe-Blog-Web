0a9c9bf45cd46d34179bf84be4985b2f99580e81
gregori bruno felicio <gregory.wow@hotmail.com>
2021-04-21 18:45:57 -0300

    create post service

---
+++ src/app/post/post.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ServiceBase } from '../shared/service.base';
+import { Post } from './post';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService extends ServiceBase {
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  create(post: Post) {
+    return this.http.post<Post>(this.url, post);
+  }
+}
