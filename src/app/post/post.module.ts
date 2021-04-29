e7397ea982ee7fccc62619877be72f3f490a7511
gregori bruno felicio <gregory.wow@hotmail.com>
2021-04-29 20:35:30 -0300

    create post list component

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PostCreateComponent } from './post-create/post-create.component';
+import { PostListComponent } from './post-list/post-list.component';
 import { PostComponent } from './post.component';
 import { PostRoutingModule } from './post.routing.module';
 
 @NgModule({
-  declarations: [PostComponent, PostCreateComponent],
+  declarations: [PostComponent, PostListComponent, PostCreateComponent],
   imports: [CommonModule, PostRoutingModule, FormsModule, ReactiveFormsModule],
 })
 export class PostModule {}
