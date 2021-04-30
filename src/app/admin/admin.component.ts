2f3a7b5eb238e8b08569d63b494ac05e022eacf7
gregori bruno felicio <gregory.wow@hotmail.com>
2021-04-29 21:20:18 -0300

    create navitagion to post creation in admin screen

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -6,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    console.log(`chamou admin`);
+  ngOnInit(): void {}
+
+  navigate() {
+    this.router.navigate(['post-create']);
   }
 }
