b2454fe38ea7a50aa3cde4e475f2178cc6ac6946
gregori bruno felicio <gregory.wow@hotmail.com>
2021-05-04 20:20:06 -0300

    fix tests

--- src/app/post/post-view/post-view.component.spec.ts
+++ src/app/post/post-view/post-view.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { PostViewComponent } from './post-view.component';
 
 describe('PostViewComponent', () => {
@@ -8,9 +9,9 @@ describe('PostViewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PostViewComponent ]
-    })
-    .compileComponents();
+      declarations: [PostViewComponent],
+      providers: [ActivatedRoute, RouterTestingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
